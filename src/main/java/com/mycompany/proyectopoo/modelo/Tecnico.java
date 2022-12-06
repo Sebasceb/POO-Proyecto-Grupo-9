059ae6ba20adc8b6e110a17acf8ecae88967d800
Sebasceb <sebastian_ceballos_1996@hotmail.com>
2022-12-05 20:51:46 -0500

    Por que me habre notepad++ esta vaina?????????? Por que me habre notepad++ esta vaina??????????

--- src/main/java/com/mycompany/proyectopoo/modelo/Tecnico.java
+++ src/main/java/com/mycompany/proyectopoo/modelo/Tecnico.java
@@ -1,5 +1,7 @@
 package modelo;
 
+import com.mycompany.proyectoprimerparcial.MenusAdministrador.Usuario;
+
 public class Tecnico extends Usuario{
   Tecnico(String usuario, String contraseña, String nombre){
     super(usuario,contraseña,nombre,"tecnico");
